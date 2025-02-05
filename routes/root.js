66cbce66bf4a2d67d7e8c8f05e6e512a0d302948
Akinola Ayobami <ayobamiakinola84@gmail.com>
2025-02-05 22:45:18 +0100

    feat: controllers and models

--- routes/root.js
+++ routes/root.js
@@ -6,14 +6,4 @@ router.get("^/$|/index(.html)?", (req, res) => {
 	res.sendFile(path.join(__dirname, "..", "views", "index.html"));
 });
 
-router.get("/new-page(.html)?", (req, res) => {
-	res.sendFile(path.join(__dirname, "..", "views", "new-page.html"));
-});
-
-router.get("/old-page(.html)?", (req, res) => {
-	res.status(301).redirect(
-		path.join(__dirname, "..", "views", "new-page.html")
-	);
-});
-
 module.exports = router;
--- server.js
+++ server.js
@@ -4,35 +4,26 @@ const cors = require("cors");
 const path = require("path");
 const { logger, logEvents } = require("./middleware/logEvents");
 const { errorHandler } = require("./middleware/errorHandler");
+const corsOptions = require("./config/corsOptions");
 
 const PORT = process.env.PORT || 3500;
 
 app.use(express.json());
 app.use(logger);
 
-const whitelist = [
-	"http://localhost:3500",
-	"http://127.0.0.1:5500",
-	"https://www.google.com",
-];
-
-const corsOptions = {
-	origin: (origin, callback) => {
-		if (whitelist.indexOf(origin) !== -1 || !origin) {
-			callback(null, true);
-		} else {
-			callback(new Error("Not allowed by CORS"));
-		}
-	},
-	optionsSuccessStatus: 200,
-};
 app.use(cors(corsOptions));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 app.use("/", require("./routes/root"));
-app.use("/subdir", require("./routes/subdir"));
+app.use("/employees", require("./routes/api/employees"));
 
 app.all("*", (req, res) => {
-	res.status(404).sendFile("./views/404.html", { root: __dirname });
+	res.status(404);
+	if (req.accepts("html")) {
+		res.sendFile(path.join(__dirname, "views", "404.html"));
+	} else if (req.accepts("json")) {
+		res.json({ error: "404 Not Found" });
+	} else res.type("txt").send("404 Not Found");
 });
 
 app.use(errorHandler);
